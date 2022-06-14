79f18b6a0d0c41c656658bf510a780dac21d0f47
Walfles <97708847+Walfles@users.noreply.github.com>
2022-06-14 13:06:09 -0700

    Fixed

--- client/cl_ped.lua
+++ client/cl_ped.lua
@@ -2,13 +2,13 @@
 
 -- Variables
 local pedSpawned = false
-local homelessmanPed = {}
+local BeerMan = {}
 
 -- createPeds function
 local function createPeds()
     if pedSpawned then return end
     for k, v in pairs(Config.Location) do
-        if not homelessmanPed[k] then homelessmanPed[k] = {} end
+        if not BeerMan[k] then BeerMan[k] = {} end
         local current = v["ped"]
         current = type(current) == 'string' and GetHashKey(current) or current
         RequestModel(current)
@@ -16,18 +16,18 @@ local function createPeds()
         while not HasModelLoaded(current) do
             Wait(0)
         end
-        homelessmanPed[k] = CreatePed(0, current, v["coords"].x, v["coords"].y, v["coords"].z-1, v["coords"].w, false, false)
-        TaskStartScenarioInPlace(homelessmanPed[k], v["scenario"], true)
-        FreezeEntityPosition(homelessmanPed[k], true)
-        SetEntityInvincible(homelessmanPed[k], true)
-        SetBlockingOfNonTemporaryEvents(homelessmanPed[k], true)
+        BeerMan[k] = CreatePed(0, current, v["coords"].x, v["coords"].y, v["coords"].z-1, v["coords"].w, false, false)
+        TaskStartScenarioInPlace(BeerMan[k], v["scenario"], true)
+        FreezeEntityPosition(BeerMan[k], true)
+        SetEntityInvincible(BeerMan[k], true)
+        SetBlockingOfNonTemporaryEvents(BeerMan[k], true)
     end
 end
 
     -- DeletePeds Function 
     local function deletePeds()
         if pedSpawned then
-            for _, v in pairs(homelessmanPed) do
+            for _, v in pairs(BeerMan) do
                 DeletePed(v)
             end
         end
