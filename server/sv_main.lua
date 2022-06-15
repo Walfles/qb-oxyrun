cc7865cdc930faf5b7df605a04bca9961154163b
Walfles <97708847+Walfles@users.noreply.github.com>
2022-06-14 17:18:10 -0700

    Checking if player has beer

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -18,3 +18,16 @@ AddEventHandler('qb-oxyrun:giveoxy', function()
     Player.Functions.AddItem('oxy', 1)
     TriggerClientEvent("inventory:client:ItemBox", source, QBCore.Shared.Items["oxy"], "add")
 end)
+
+-- If Player Has Beer
+QBCore.Functions.CreateCallback('qb-oxyrun:hasBeer', function(source, cb)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player ~= nil then
+        local HasBeer = Player.Functions.GetItemByName("beer")
+        if HasBeer ~= nil then
+            cb(true)
+        else
+            cb(false)
+        end
+    end
+end)
